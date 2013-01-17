e7492716c0fba7b39a4f98c4b0710bf3783f1aa5
bmcorser <benmarshallcorser@gmail.com>
2013-01-17 22:16:54 +0000

    home

--- originalenclosure/originalenclosure/urls.py
+++ originalenclosure/originalenclosure/urls.py
@@ -7,7 +7,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'originalenclosure.home', name='originalenclosure.home'),
+    url(r'^$', 'originalenclosure.views.home', name='home'),
     url(r'^pars/', include('pars.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
