d51f4b0ddfbbc6098cdcc9f578373f1ecc0ebcf7
bmcorser <benmarshallcorser@gmail.com>
2013-01-21 18:54:05 +0000

    social beginnings

---
+++ originalenclosure/pars/management/commands/tweet.py
@@ -0,0 +1,17 @@
+import re, tweepy
+from datetime import date
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from pars.models import Par
+
+class Command(BaseCommand):
+  help = "Post to twitter"
+
+  def __init__(self,*args,**kwargs):
+    pass
+
+  def handle(self, *args, **kwargs):
+    auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(settings.TWITTER_ACCESS_KEY, settings.TWITTER_ACCESS_SECRET)
+    api = tweepy.API(auth)
+    api.update_status('test')
