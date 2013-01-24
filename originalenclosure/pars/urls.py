db8f42d125fc5029f28c54ba3246c81a347ae82c
bmcorser <benmarshallcorser@gmail.com>
2013-01-24 22:58:26 +0000

    better urlz

--- originalenclosure/pars/urls.py
+++ originalenclosure/pars/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('pars.views',
     url(r'^$', 'par', name='par' ),
-    url(r'^edit/(?P<par>\d+)/swap', 'swap', name='swap'),
+    url(r'^swap/(?P<par>\d+)', 'swap', name='swap'),
     url(r'^edit/(?P<par>\d+)?', 'edit', name='edit'),
     url(r'^make$', 'make', name='make'),
     url(r'^(?P<par>\d+)', 'par', name='par'),
