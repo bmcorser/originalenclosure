5b60942808a9efc27b8d8f9fd1cc9455ea7f742f
bmcorser <benmarshallcorser@gmail.com>
2013-08-22 00:22:41 +0100

    who knows

--- originalenclosure/pars/management/commands/see.py
+++ originalenclosure/pars/management/commands/see.py
@@ -25,19 +25,8 @@ class Command(BaseCommand):
     help = "Sees if par images are available at source"
 
     def handle(self, *args, **kwargs):
-        text = urwid.Text('yeah')
-        fill = urwid.Filler(text, 'middle')
-        pars = []
-        def grab_pars(loop, *args):
-            for par in Par.objects.all()[:20]:
-                parsee = ParSee()
-                parsee.left_seen = seen.delay(par.left)
-                parsee.right_seen = seen.delay(par.right)
-                pars.append(parsee)
-            loop.set_alarm_in(1, change_text)
-        loop = urwid.MainLoop(urwid.Filler(urwid.Text(unicode([str(par) for par in pars])), 'middle'))
-        def change_text(loop, *args, **kwargs):
-            loop.widget = urwid.Filler(urwid.Text([unicode(par) for par in pars]), 'middle')
-            loop.set_alarm_in(1, change_text)
-        grab_pars(loop)
-        loop.run()
+        for par in Par.objects.all():
+            parsee = ParSee()
+            parsee.left_seen = seen.delay(par.left)
+            parsee.right_seen = seen.delay(par.right)
+            print parsee.__unicode__()
