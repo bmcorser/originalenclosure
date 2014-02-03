cde316d46709f279f7ef1e8f5f3e79e1c8b2879c
bmcorser <benmarshallcorser@gmail.com>
2014-02-03 20:36:42 +0000

    Sucking on an empty can of Guinness

--- originalenclosure/originalenclosure/settings/dev.py
+++ originalenclosure/originalenclosure/settings/dev.py
@@ -1,5 +1,14 @@
 from .base import *
 DOMAIN = 'http://www.originalenclosure.net:8000'
+nest = join(*2*['..'])
+ROOT = abspath(join(dirname(__file__), nest))
+TOP_FOLDER = join(ROOT, 'originalenclosure')
+
+MEDIA_ROOT = join(ROOT, 'media')
+STATIC_ROOT = join(ROOT, 'static')
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
 DEBUG = True
 DATABASES = {
     'default': {
