bb4ee64bd4423a89ad48bb03dd35cb8cd8c30beb
bmcorser <benmarshallcorser@gmail.com>
2016-02-06 11:11:34 +0000

    Follow the fb

--- originalenclosure/pars/tasks.py
+++ originalenclosure/pars/tasks.py
@@ -2,6 +2,8 @@
 import os, re
 from time import sleep
 
+from celery import task
+import facebook as fb_api
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -10,9 +12,6 @@ from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
 
 from pars.models import Par, Image
-
-from celery import task
-
 from .purchases import make_pdf, make_png, make_gumroad_product
 
 TIMEOUT = 30
@@ -21,6 +20,17 @@ TIMEOUT = 30
 def sleep_task(n):
     sleep(n)
     return 'sleeping completed'
+
+def encoded_dict(in_dict):
+    out_dict = {}
+    for k, v in in_dict.iteritems():
+        if isinstance(v, unicode):
+            v = v.encode('utf8')
+        elif isinstance(v, str):
+            # Must be encoded in UTF-8
+            v.decode('utf8')
+        out_dict[k] = v
+    return out_dict
 
 @task
 def facebook():
@@ -39,10 +49,12 @@ def facebook():
         }
 
         par_buffer = Par.objects.filter(in_buffer=True) 
+        picture = None
         for par in par_buffer:
             if par.hidden:
                 blank_words = [len(word)*'█' for word in par.title.split(' ')]
                 par.title = ' '.join(blank_words)
+            picture = 'http://originalenclosure.net' + par.left.image.url
 
         message_dict = {
             'pars': par_buffer
@@ -51,20 +63,23 @@ def facebook():
         description = render_to_string('facebook/description.html',
                                        description_dict)
         message = render_to_string('facebook/message.html', message_dict)
-        link = ''.join(['http://www.originalenclosure.net',reverse('parhome')])
+        link = ''.join(['http://originalenclosure.net',reverse('parhome')])
         payload = {
             'name': 'Pars, 2010 © Ben Marshall-Corser.',
+            'link': link,
             'caption': 'New pars!',
             'description': description,
-            'access_token': settings.FACEBOOK_ACCESS_TOKEN,
-            'app_id': settings.FACEBOOK_APP_ID,
-            'link': link,
-            'message': message,
+            'picture': picture,
         }
-        r = requests.post('https://graph.facebook.com/benmarshallcorser/feed/',
-                          data=payload)
-        print r.text
-        if r.status_code == 200:
+        graph = fb_api.GraphAPI(settings.FACEBOOK_ACCESS_TOKEN)
+        resp = graph.put_wall_post(
+            message=message.encode('utf8'),
+            attachment=encoded_dict(payload)
+        )
+        print resp
+        '''
+        if 'id' in resp:  # assume everything's ok
             Par.objects.filter(in_buffer=True).update(in_buffer=False)
+        '''
     else:
         print 'nothing to do'
